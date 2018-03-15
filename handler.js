30eb0bba3d59f8cac15c6d9f815853d4d48805db
celeron1ghz <celeron1ghz@gmail.com>
2018-03-15 06:02:52 +0000

    impl in handler.js

--- handler.js
+++ handler.js
@@ -1,5 +1,34 @@
 'use strict';
 
+const vo = require('vo');
+
+const config = [
+  { channel: 'hibiki', programId: 'llss' },
+];
+
+const CHANNELS = {
+  hibiki: require('./src/fetcher/HibikiRadioFetcher'),
+};
+
 module.exports.fetch = (event, context, callback) => {
-  callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!' });
+  vo(function*(){
+    for (const c of config) {
+      const fetcher = CHANNELS[c.channel];
+
+      if (!fetcher) {
+        console.log(`Error: unknown channel '${c.channel}'. skipped...`);
+        continue;
+      }
+
+      yield new fetcher(c).fetch().then(data => {
+        console.log(c.channel, ": ok");
+      });
+    }
+
+    callback(null, "OK");
+  })
+  .catch(err => {
+    console.log("Error:", err);
+    callback(err);
+  });
 };
