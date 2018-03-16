f8d807ae2ee774242b2696f3c509bebeebdf245d
celeron1ghz <celeron1ghz@gmail.com>
2018-03-16 02:00:08 +0000

    move to parent

--- src/common/Fetcher.js
+++ src/common/Fetcher.js
@@ -1,6 +1,12 @@
+const debug = require('debug')('koebutter.fetcher');
 const fs = require("fs");
+const vo = require('vo');
 const aws = require('aws-sdk');
 const s3  = new aws.S3();
+
+const Recorder = require('../recorder/FfmpegRecorder');
+
+const __PROGRAM_LIST_CACHE = {};
 
 class Fetcher {
   constructor(args) {
@@ -19,7 +25,53 @@ class Fetcher {
   }
 
   fetch() {
-    throw new Error("impl fetch()!!!!");
+    const self = this;
+
+    return vo(function*(){
+      const pid = self.programId;
+      const clazz = self.constructor.name;
+
+      // get program list
+      if (!__PROGRAM_LIST_CACHE[clazz]) {
+        __PROGRAM_LIST_CACHE[clazz] = yield self.fetch_program_list();
+      }
+
+
+      // pid exist check
+      const matched = __PROGRAM_LIST_CACHE[clazz].filter(p => p.access_id === pid);
+
+      if (matched.length === 0) {
+        console.log(`SKIP: ID '${pid}' is not exist.`);
+        return;
+      }
+
+
+      // recorded file exist check
+      const f = yield self.get_filename(matched[0]);
+
+      if (yield self.fileExists(f.remoteFile)) {
+        console.log("file already recorded.");
+        return;
+      }
+
+
+      // recording...
+      const param = yield self.get_recorder_params(f);
+      debug("RECORDER_RET", yield Recorder.record(pid, f.localFile, param));
+
+
+      // put recorded file to s3...
+      const stream = fs.createReadStream(f.localFile);
+      debug("S3_VIDEO_RET", yield self.publish(f.remoteFile, stream));
+      debug("S3_INFO_RET",  yield self.publish(f.remoteFile + ".json", JSON.stringify(f.program)));
+    })
+    .catch(err => {
+      if (err.response) {
+        throw new Error(err.response.error.message); // network error
+      } else {
+        throw err; // other error
+      }
+    });
   }
 }
 
--- src/fetcher/HibikiRadioFetcher.js
+++ src/fetcher/HibikiRadioFetcher.js
@@ -1,14 +1,9 @@
-const fs = require("fs");
-const vo = require('vo');
+const debug = require('debug')('koebutter.fetcher.hibiki');
 const tempy = require('tempy');
 const request = require('superagent');
 const { sprintf } = require("sprintf-js");
-const debug = require('debug')('koebutter.fetcher.hibiki');
 
 const Fetcher = require('../common/Fetcher');
-const recorder = require('../recorder/FfmpegRecorder');
-
-const __PROGRAM_LIST_CACHE = {};
 
 class HibikiRadioFetcher extends Fetcher {
   constructor(args) {
@@ -63,57 +58,7 @@ class HibikiRadioFetcher extends Fetcher {
         '-acodec', 'copy',
         '-bsf:a', 'aac_adtstoasc',
         localFile,
-      ])
-  }
-
-  fetch() {
-    const self = this;
-
-    return vo(function*(){
-      const pid = self.programId;
-      const clazz = self.constructor.name;
-
-      // get program list
-      if (!__PROGRAM_LIST_CACHE[clazz]) {
-        __PROGRAM_LIST_CACHE[clazz] = yield self.fetch_program_list();
-      }
-
-
-      // pid exist check
-      const matched = __PROGRAM_LIST_CACHE[clazz].filter(p => p.access_id === pid);
-
-      if (matched.length === 0) {
-        console.log(`SKIP: ID '${pid}' is not exist.`);
-        return;
-      }
-
-
-      // recorded file exist check
-      const f = yield self.get_filename(matched[0]);
-
-      if (yield self.fileExists(f.remoteFile)) {
-        console.log("file already recorded.");
-        return;
-      }
-
-
-      // recording...
-      const param = yield self.get_recorder_params(f);
-      debug("RECORDER_RET", yield recorder.record(pid, f.localFile, param));
-
-
-      // put recorded file to s3...
-      const stream = fs.createReadStream(f.localFile);
-      debug("S3_VIDEO_RET", yield self.publish(f.remoteFile, stream));
-      debug("S3_INFO_RET",  yield self.publish(f.remoteFile + ".json", JSON.stringify(f.program)));
-    })
-    .catch(err => {
-      if (err.response) {
-        throw new Error(err.response.error.message); // network error
-      } else {
-        throw err; // other error
-      }
-    });
+      ]);
   }
 }
 
