eaac943b76d18a7dfacbfb320a3c514910dc0707
celeron1ghz <celeron1ghz@gmail.com>
2018-03-22 00:39:24 +0000

    separated

--- handler.js
+++ handler.js
@@ -1,57 +1,5 @@
 'use strict';
 
-const vo = require('vo');
-const debug = require('debug')('koebutter');
+const Fetch = require('./src/handler/fetch');
 
-const config = [
-  { channel: 'hibiki', programId: 'llss' },
-  { channel: 'hibiki', programId: 'lovelive-ms' },
-  { channel: 'hibiki', programId: 'sora' },
-  { channel: 'hibiki', programId: 'revuestarlight' },
-  { channel: 'onsen',  programId: 'aya-uchida' },
-  { channel: 'onsen',  programId: 'ff' },
-  { channel: 'onsen',  programId: 'garupa' },
-  { channel: 'onsen',  programId: 'mogucomi' },
-];
-
-const CHANNELS = {
-  hibiki: require('./src/fetcher/HibikiRadioFetcher'),
-  onsen:  require('./src/fetcher/OnsenFetcher'),
-};
-
-module.exports.fetch = (event, context, callback) => {
-  debug("handler.vo() GENERATE");
-  vo(function*(){
-    debug("handler.vo() START");
-
-    for (const c of config) {
-      debug("handler.vo() LOOP_START");
-
-      const fetcher = CHANNELS[c.channel];
-
-      if (!fetcher) {
-        console.log(`Error: unknown channel '${c.channel}'. skipped...`);
-        debug("handler.vo() CONTINUE_UNKNOWN_CHANNEL");
-        continue;
-      }
-
-      try {
-        yield new fetcher(c).fetch().catch(err => {
-          console.log("ERROR:", err.message);
-        });
-      } catch(err) {
-        console.log("Error on " + c.channel + ":", err);
-      }
-
-      debug("handler.vo() LOOP_END");
-    }
-
-    debug("handler.vo() LAST_LINE");
-    callback(null, "OK");
-    return null;
-  })
-  .catch(err => {
-    console.log("Uncaught error:", err);
-    callback(err);
-  });
-};
+module.exports.fetch = Fetch.fetch;
